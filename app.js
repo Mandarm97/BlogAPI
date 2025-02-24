0b6d44522f8e6a396fc3354063e4a81cd5d585f6
Mandarm97 <mandarm9977@gmail.com>
2025-02-24 11:28:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/api/user' ,userRoute);
 
 
 async function main(){
-    const res = await mongoose.connect('mongodb+srv://mandarm9977:OMmyFamily2604@cluster0.pkqzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    const res = await mongoose.connect('mongodb+srv://mandarm9977:@cluster0.pkqzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     const data = await res.default
     const response = data.STATES['1']
     console.log(response);
